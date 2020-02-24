c9aac67ed0166288f55113a4c46b5a50c372bd17
andy3_kuo <andy3_kuo@asus.com>
2020-02-24 17:09:49 +0800

    power: reset: imx-snvs-poweroff: add power off driver for PE100A
    set LPCR ON_TIME register to 0ms at power off driver
    
    Change-Id: Id8c5b2d63d6ec5f9cfa142e756cfaefc36b7401c

--- drivers/power/reset/imx-snvs-poweroff.c
+++ drivers/power/reset/imx-snvs-poweroff.c
@@ -22,19 +22,41 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
+#include <linux/mfd/syscon.h>
+#include <linux/regmap.h>
+
+#define SNVS_LPCR_REG	0x38    /* LP Control Register */
+#define SNVS_LPCR_ON	BIT(20) | BIT(21)
+#define SNVS_TOP_DP	BIT(6) | BIT(5)
+
+#define ON_TIME_500MS	0x0 << 20
+#define ON_TIME_50MS	0x1 << 20
+#define ON_TIME_100MS	0x2 << 20
+#define ON_TIME_0MS	0x3 << 20
+#define TOP_DP_EN	0x3 << 5
 
 static void __iomem *snvs_base;
 
+struct poweroff_drv_data {
+	struct regmap *snvs;
+};
+
+struct poweroff_drv_data *pdata = NULL;
+
 static void do_imx_poweroff(void)
 {
-	u32 value = readl(snvs_base);
+//	u32 value = readl(snvs_base);
 
 	/* set TOP and DP_EN bit */
-	writel(value | 0x60, snvs_base);
+//	writel(value | 0x60, snvs_base);
+
+	regmap_update_bits(pdata->snvs, SNVS_LPCR_REG, SNVS_LPCR_ON, ON_TIME_50MS);
+	regmap_update_bits(pdata->snvs, SNVS_LPCR_REG, SNVS_TOP_DP, TOP_DP_EN);
 }
 
 static int imx_poweroff_probe(struct platform_device *pdev)
 {
+/*
 	snvs_base = of_iomap(pdev->dev.of_node, 0);
 	if (!snvs_base) {
 		dev_err(&pdev->dev, "failed to get memory\n");
@@ -42,6 +64,29 @@ static int imx_poweroff_probe(struct platform_device *pdev)
 	}
 
 	pm_power_off = do_imx_poweroff;
+*/
+	struct device_node *np;
+
+	/* Get SNVS register Page */
+	np = pdev->dev.of_node;
+	if (!np)
+		return -ENODEV;
+
+	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
+	if (!pdata)
+		return -ENOMEM;
+
+	pdata->snvs = syscon_regmap_lookup_by_phandle(np, "regmap");
+	if (IS_ERR(pdata->snvs)) {
+		dev_err(&pdev->dev, "Can't get snvs syscon\n");
+		return PTR_ERR(pdata->snvs);
+	}
+
+	/* set ON_TIME to 50 sec */
+	regmap_update_bits(pdata->snvs, SNVS_LPCR_REG, SNVS_LPCR_ON, ON_TIME_50MS);
+
+	pm_power_off = do_imx_poweroff;
+
 	return 0;
 }
 
