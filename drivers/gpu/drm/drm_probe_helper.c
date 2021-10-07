3b269ae1f823d68e0b955f19bc447caedff2f44f
bross1_kuo <bross1_kuo@asus.com>
2021-10-07 11:08:37 +0800

    HDMI: modify resolution upper limmit to 1920x1080 when noEDID
    add 720x480, 1280x720, 1920x1080 to drm_dmt_modes table for noEDID use
    
    Change-Id: I5427cf834b9af44adde8f1c2e3dbcafd9cca0e1a
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -516,7 +516,7 @@ retry:
 		count = drm_add_override_edid_modes(connector);
 
 	if (count == 0 && connector->status == connector_status_connected)
-		count = drm_add_modes_noedid(connector, 1024, 768);
+		count = drm_add_modes_noedid(connector, 1920, 1080);
 	count += drm_helper_probe_add_cmdline_mode(connector);
 	if (count == 0)
 		goto prune;
