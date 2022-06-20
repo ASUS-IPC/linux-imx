1f139e30918f98af87d0ed83cc16d6d4fbbf5870
Rock Shen <rock_shen@asus.com>
2022-06-20 08:56:11 +0800

    Ethernet: rtl8211e driver porting for PE100A
    Signed-off-by: Rock Shen <rock_shen@asus.com>
    Change-Id: I99f2cb1eb1f7703da2a1637126b2d18917e95205

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -26,8 +26,14 @@
 
 #define RTL8211F_INER_LINK_STATUS 0x0010
 #define RTL8211F_INSR		0x1d
-#define RTL8211F_PAGE_SELECT	0x1f
+#define RTL8211X_PAGE_SELECT	0x1f
 #define RTL8211F_TX_DELAY	0x100
+#define RTL8211E_EXT_PAGE_SELECT	0x1e
+#define RTL8211E_EXT_PAGE	0x7
+#define RTL8211E_CONFIG_REG	0x1c	/* Configuration register */
+#define RTL8211E_CTRL_DELAY	0x2000
+#define RTL8211E_TX_DELAY	0x1000
+#define RTL8211E_RX_DELAY	0x0800
 
 MODULE_DESCRIPTION("Realtek PHY driver");
 MODULE_AUTHOR("Johnson Leung");
@@ -46,10 +52,10 @@ static int rtl8211f_ack_interrupt(struct phy_device *phydev)
 {
 	int err;
 
-	phy_write(phydev, RTL8211F_PAGE_SELECT, 0xa43);
+	phy_write(phydev, RTL8211X_PAGE_SELECT, 0xa43);
 	err = phy_read(phydev, RTL8211F_INSR);
 	/* restore to default page 0 */
-	phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0);
+	phy_write(phydev, RTL8211X_PAGE_SELECT, 0x0);
 
 	return (err < 0) ? err : 0;
 }
@@ -102,7 +108,7 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 	if (ret < 0)
 		return ret;
 
-	phy_write(phydev, RTL8211F_PAGE_SELECT, 0xd08);
+	phy_write(phydev, RTL8211X_PAGE_SELECT, 0xd08);
 	reg = phy_read(phydev, 0x11);
 
 	/* enable TX-delay for rgmii-id and rgmii-txid, otherwise disable it */
@@ -114,11 +120,24 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 
 	phy_write(phydev, 0x11, reg);
 	/* restore to default page 0 */
-	phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0);
+	phy_write(phydev, RTL8211X_PAGE_SELECT, 0x0);
 
 	return 0;
 }
 
+static int rtl8211e_config_init(struct phy_device *phydev)
+{
+	u16 reg;
+
+	phy_write(phydev, RTL8211X_PAGE_SELECT, RTL8211E_EXT_PAGE);
+	phy_write(phydev, RTL8211E_EXT_PAGE_SELECT, 0xa4);
+
+	reg = phy_read(phydev, RTL8211E_CONFIG_REG);
+	reg |= (RTL8211E_CTRL_DELAY | RTL8211E_TX_DELAY | RTL8211E_RX_DELAY);
+	phy_write(phydev, RTL8211E_CONFIG_REG, reg);
+	phy_write(phydev, RTL8211X_PAGE_SELECT, 0x0);
+	return 0;
+}
 static struct phy_driver realtek_drvs[] = {
 	{
 		.phy_id         = 0x00008201,
@@ -155,6 +174,7 @@ static struct phy_driver realtek_drvs[] = {
 	}, {
 		.phy_id		= 0x001cc915,
 		.name		= "RTL8211E Gigabit Ethernet",
+		.config_init    = &rtl8211e_config_init,
 		.phy_id_mask	= 0x001fffff,
 		.features	= PHY_GBIT_FEATURES,
 		.flags		= PHY_HAS_INTERRUPT,
