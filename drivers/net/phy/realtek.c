838ec0bd70e6619feb2b50e15f24f3346ca0e6ba
yupei_lin <YuPei_Lin@asus.com>
2023-06-15 09:22:22 +0800

    Ethernet : move TX-Delay to applications for RTL9010ARG.
    Change-Id: I5b8b613404445b268040d45e97a892731bc53b7c

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -779,6 +779,19 @@ static int RTL9010AA_VA_Initial_with_NWAY_Configuration(struct phy_device *phyde
 	}
 	// End //
 
+	phy_write(phydev, 0x0, 0x8000); // PHY soft-reset
+	mdio_data = 0;
+
+	while (mdio_data != 0x0140){    // Check soft-reset complete
+		mdio_data = phy_read(phydev, 0x0);
+	}
+
+	pr_info("%s ---\n", __func__);
+	return SUCCESS;
+}
+
+static int RTL9010AA_VA_ADD_TX_DELAY(struct phy_device *phydev)
+{
 	pr_info("RTL9010ARG : RGMII Timing Control : TXC latching TXD = 00 : Speed of adjusting TXC delay = 11\n");
 	phy_write(phydev, 0x1B, 0xD084);
 	pr_info("RTL9010ARG : RGTR2 : 0xD084(default) = 0x%x\n", phy_read(phydev, 0x1C));
@@ -800,16 +813,9 @@ static int RTL9010AA_VA_Initial_with_NWAY_Configuration(struct phy_device *phyde
 	phy_write(phydev, 0x1B, 0xD084);
 	phy_write(phydev, 0x1C, 0x0007);
 
-	phy_write(phydev, 0x0, 0x8000); // PHY soft-reset
-	mdio_data = 0;
-
-	while (mdio_data != 0x0140){    // Check soft-reset complete
-		mdio_data = phy_read(phydev, 0x0);
-	}
-
-	pr_info("%s ---\n", __func__);
-	return SUCCESS;
+	return 0;
 }
+
 static int RTL9010AA_VA_RGMII_driving_strength(struct phy_device *phydev)
 {
 	// Weak_RGMII_1V8
@@ -867,6 +873,7 @@ static int rtl9010aa_config_init(struct phy_device *phydev)
 	RTL9010AA_VA_Initial_with_NWAY_Configuration(phydev);
 	phy_write_paged(phydev, 0xa4c, 0x12, 0x20ff); // 1v8
 	pr_info("RTL9010ARG : io power select 1v8 = 0x%x\n", phy_read_paged(phydev, 0xa4c, 0x12));
+	RTL9010AA_VA_ADD_TX_DELAY(phydev);
 	RTL9010AA_VA_RGMII_driving_strength(phydev);
 	RTL9010AA_VA_Soft_Reset(phydev);
 	return 0;
