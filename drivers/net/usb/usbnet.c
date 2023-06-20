bc510c71825c18750e291e5357802ccdbb1e277e
YiowHur_Ng <yiowhur_ng@asus.com>
2023-06-20 16:29:52 +0800

    LTE: Blink LED when transmit data
    Change-Id: Ic466289d0f02df995aeaeb17518e735a9a911cbb
    Signed-off-by: YiowHur_Ng <yiowhur_ng@asus.com>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -33,6 +33,7 @@
 #include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/pm_runtime.h>
+#include <linux/leds.h>
 
 /*-------------------------------------------------------------------------*/
 
@@ -1360,6 +1361,8 @@ netdev_tx_t usbnet_start_xmit (struct sk_buff *skb,
 	const struct driver_info *info = dev->driver_info;
 	unsigned long		flags;
 	int retval;
+
+	ledtrig_led_func_blink_oneshot(LED_FUNC_LTE, LED_LIGHT_GREEN, 50, 50, 0);
 
 	if (skb)
 		skb_tx_timestamp(skb);
