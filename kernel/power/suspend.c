e062d670bad364f395b1fd1f03b055d766a48be9
Kunyang_Fan <Kunyang_Fan@asus.com>
2023-09-11 16:18:55 +0800

    Power: Disable "freeze" (s2idle) state in system
    Since the PCIE hub will resume fail after resuming from "s2Idle" mode.
    Disable this option "freeze" in default to prevent PCIE devices connected
    to the hub from malfunction and device lost.
    
    Change-Id: I656a6605148a15f2df377eac313a9238df182228
    Signed-off-by: Kunyang_Fan <Kunyang_Fan@asus.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -171,7 +171,8 @@ void __init pm_states_init(void)
 {
 	/* "mem" and "freeze" are always present in /sys/power/state. */
 	pm_states[PM_SUSPEND_MEM] = pm_labels[PM_SUSPEND_MEM];
-	pm_states[PM_SUSPEND_TO_IDLE] = pm_labels[PM_SUSPEND_TO_IDLE];
+	/* Disable the freeze option for the devices which has PCIE hub*/
+	//pm_states[PM_SUSPEND_TO_IDLE] = pm_labels[PM_SUSPEND_TO_IDLE];
 	/*
 	 * Suspend-to-idle should be supported even without any suspend_ops,
 	 * initialize mem_sleep_states[] accordingly here.
